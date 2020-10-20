d1f94db37e72360e3edc7f250133231b57e134c8
goalsgo1 <36186769+goalsgo1@users.noreply.github.com>
2020-10-20 19:28:36 +0900

    A

--- vamos/src/breedlove/GithubTest.java
+++ vamos/src/breedlove/GithubTest.java
@@ -3,21 +3,7 @@ package breedlove;
 public class GithubTest {
 
 	public static void main(String[] args) {
-		//전영문1020
-<<<<<<< HEAD
-		//이해민1020
-		//qkqhqkqh
-=======
-		//이해민바보
-<<<<<<< HEAD
-		//종희선입니다.러ㅇㅇㅇ
-=======
->>>>>>> c30f90e00dd0b199cdf3879698bfc21a7e873144
-//♥
-//신고
-//주석 
->>>>>>> branch 'master' of https://github.com/Myrtlewood/Vamos
-	////////fdlfkhlskhsf 7시18
+		//A
 	}
 
 }
